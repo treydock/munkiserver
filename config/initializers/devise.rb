c1778ed53819282f4193a4706d69ef1c3d2eba03
Trey Dockendorf <treydock@tamu.edu>
2012-03-08 19:41:21 -0600

    - change cas server

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,7 +198,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :cas, :host => 'cas.tamu.edu/cas', :ssl => true
+  config.omniauth :cas, :host => 'cas-dev.tamu.edu/cas', :ssl => true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
